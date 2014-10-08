aaeb481946e812a8787d2b35bfc6b303e5e05d9e
barraudf <barraudf@gmail.com>
2014-10-08 23:49:26 +0200

    Fix Left/Right bounds set for the wrong player

--- Assets/Scripts/ScroreController.cs
+++ Assets/Scripts/ScroreController.cs
@@ -5,7 +5,6 @@ public class ScroreController : MonoBehaviour
 {
     public GameController.Players Player;
 
-    public bool characterInQuicksand;
     void OnTriggerEnter2D(Collider2D other)
     {
         GameController.Instance.Score(Player);
