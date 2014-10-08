20cf9bb7b266f825f1cacf3770b71d7c0b2d25ca
barraudf <barraudf@gmail.com>
2014-10-08 23:47:04 +0200

    Score display added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameController : Singleton<GameController>
@@ -8,22 +9,37 @@ public class GameController : Singleton<GameController>
     public float GameSpeedCoefficient = 1.0f;
 
     private int _LeftPlayerScore = 0;
-    private int _RightPlayer = 0;
+    private int _RightPlayerScore = 0;
 
     private BallController _BallController;
+
+    private Text _LeftScoreText;
+    private Text _RightScoreText;
 
     void Start()
     {
         GameObject ballGO = (GameObject)GameObject.Find("Ball");
         _BallController = ballGO.GetComponent<BallController>();
+
+        GameObject leftScoreGO = (GameObject)GameObject.Find("LeftScore");
+        _LeftScoreText = leftScoreGO.GetComponent<Text>();
+
+        GameObject RightScoreGO = (GameObject)GameObject.Find("RightScore");
+        _RightScoreText = RightScoreGO.GetComponent<Text>();
     }
 
     public void Score(Players player)
     {
         if (player == Players.LeftPlayer)
+        {
             _LeftPlayerScore++;
+            _LeftScoreText.text = _LeftPlayerScore.ToString();
+        }
         else
-            _RightPlayer++;
+        {
+            _RightPlayerScore++;
+            _RightScoreText.text = _RightPlayerScore.ToString();
+        }
         _BallController.ResetPositionAndDirection();
     }
 
